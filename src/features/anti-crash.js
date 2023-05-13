86c76264a71b10fd3af5c326606743893053c579
Sadness <95174481+RamaWasLoved@users.noreply.github.com>
2023-05-13 06:48:21 -0700

    Create anti-crash.js

---
+++ src/features/anti-crash.js
@@ -0,0 +1,49 @@
+const { EventEmitter } = require('events');
+const colors = require("colors");
+
+class AntiCrash extends EventEmitter {
+    antiCrash_Emit = this
+    constructor(client, options) {
+        super();
+        this.client = client
+        this.options = options
+
+        client.on(`ready`, async () => {
+            console.log(`> [utilcord-anticrash] loaded`.green)
+            if (!client) {
+            throw new ReferenceError(`> [utilcord-anticrash]: discord.client is invalid!`.red)
+            } else if (!options.enableAntiCrash) {
+                throw new TypeError(`> [utilcord-anticrash]: Option antiCrash is blank / has no value!`.red)
+            } else if (options.enableAntiCrash == "false") {
+                console.log(`> [utilcord-anticrash]: Option enableAntiCrash is disabled, and won't be preventing bot termination`.red)
+            } else if (options.enableAntiCrash == "true") {
+                catching()
+            } else {
+                throw new SyntaxError(`> [utilcord-anticrash]: is not valid!`)
+            }
+        })
+
+        async function catching() {
+            console.log(`> [utilcord-anticrash]: is now preventing bot termination`.blue);
+            process.on("unhandledRejection", (reason, p) => {
+                console.log(`> [utilcord-anticrash]: Unhadled Rejection/Catch`.red);
+                console.log(reason, p);
+            });
+            process.on("uncaughtException", (err, origin) => { 
+                console.log(`> [utilcord-anticrash]: Uncaught Exception/Catch (Monitor)`.red);
+                console.log(err, origin);
+            });
+            process.on("uncaughtExceptionMonitor", (err, origin) => {
+                console.log(`> [utilcord-anticrash]: Uncaught Exception/Catch (Monitor)`.red);
+                console.log(err, origin);
+            })
+            process.on("multipleResolves", (type, promise, reason) => {
+                console.log(`> [utilcord-anticrash]: Multiple Resloves`.red);
+                console.log(type, promise, reason);
+            })
+        }
+
+    }
+}
+
+module.exports = { AntiCrash };
